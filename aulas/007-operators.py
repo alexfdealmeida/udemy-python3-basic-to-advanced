a44b28571798cc01b30e87ced560d5b3ece0fa7b
alexfdealmeida <alexferreiradealmeida@gmail.com>
2025-05-03 19:11:26 -0300

    Branch: main; Added 007-operators.py

---
+++ aulas/007-operators.py
@@ -0,0 +1,15 @@
+soma = 2 + 2
+subracao = 2 - 2
+multiplicacao = 2 * 2
+exponenciacao = 2 ** 2
+divisao = 5 / 2
+divisao_inteira = 5 // 2
+divisao_modulo = 5 % 2
+
+print("Soma:", soma)
+print("Subtração:", subracao)
+print("Multiplicação:", multiplicacao)
+print("Exponenciação:", exponenciacao)
+print("Divisão:", divisao)
+print("Divisão inteira (quociente):", divisao_inteira)
+print("Divisão módulo (resto):", divisao_modulo)
