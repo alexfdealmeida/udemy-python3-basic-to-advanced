a19f133b507a6dbe2dc1cc4cd2884b5a0cd00df4
alexfdealmeida <alexferreiradealmeida@gmail.com>
2025-05-03 18:39:39 -0300

    Branch: main; Added 005-types-convertion.py

---
+++ aulas/005-types-convertion.py
@@ -0,0 +1,9 @@
+# Tipos imutáveis e primitivos: str, float e bool
+print(1 + 1)
+print("a" + "b")
+
+# Typecasting
+print(int('1') + 1)
+print(float('1.0') + 1)
+print(bool(' '))
+print(str(11) + 'b')
