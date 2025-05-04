3aa4528ff81d7cf055434a462a8263a64dcfdd2b
alexfdealmeida <alexferreiradealmeida@gmail.com>
2025-05-04 13:10:58 -0300

    Branch: main; Added 01-formatting-f-strings.py

---
+++ aulas/005-formatting/01-formatting-f-strings.py
@@ -0,0 +1,10 @@
+name = "Alex"
+height = 1.78
+weight = 68
+
+imc = weight / (height ** 2)
+
+print(f"Name: {name}")
+print(f"Height: {height:.2f}")
+print(f"Weight: {weight:.2f}")
+print(f"IMC: {imc:.2f}")
