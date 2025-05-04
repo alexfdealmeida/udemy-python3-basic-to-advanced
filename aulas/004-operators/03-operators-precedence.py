f827e26dbf6b1b8f4667231a834147c588369a05
alexfdealmeida <alexferreiradealmeida@gmail.com>
2025-05-04 11:52:15 -0300

    Branch: main; Added 004-operators/03-operators-precedence.py

---
+++ aulas/004-operators/03-operators-precedence.py
@@ -0,0 +1,12 @@
+# 1. (x + y)
+# 2. **
+# 3. * / // %
+# 4. + -
+
+operation_1 = 1 + 1 ** 5 + 5 # 7
+operation_2 = (1 + int(0.5 + 0.5)) ** 5 + 5 # 37
+operation_3 = (1 + int(0.5 + 0.5)) ** (5 + 5) # 1024
+
+print(operation_1)
+print(operation_2)
+print(operation_3)
