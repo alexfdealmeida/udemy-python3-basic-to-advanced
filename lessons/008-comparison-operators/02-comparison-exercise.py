786fbc91cd900fdc9979f7649aea48361eae2eeb
alexfdealmeida <alexferreiradealmeida@gmail.com>
2025-05-31 20:51:21 -0300

    Branch: main; Added 02-comparison-exercise.py

---
+++ lessons/008-comparison-operators/02-comparison-exercise.py
@@ -0,0 +1,7 @@
+first_value = input("Enter the first value: ")
+second_value = input("Enter the second value: ")
+
+if first_value >= second_value:
+	print(f"{first_value=} is greater than or equal "f"{second_value=}")
+else:
+	print(f"{second_value=} is greater than "f"{first_value=}")
