ba35e78fd3baef1ef9400e351d0897d7dfafec1b
alexfdealmeida <alexferreiradealmeida@gmail.com>
2025-05-31 20:41:41 -0300

    Branch: main; Added 01-comparison.py

---
+++ lessons/008-comparison-operators/01-comparison.py
@@ -0,0 +1,13 @@
+greater_than = 2 > 1
+greater_than_or_equal = 2 >= 2
+less_than = 1 < 2
+less_than_or_equal = 2 <= 2
+equal = "a" == "a"
+different = "a" != "b"
+
+print("gt:", greater_than)
+print("ge:", greater_than_or_equal)
+print("lt:", less_than)
+print("le:", less_than_or_equal)
+print("==:", greater_than)
+print("!=:", greater_than)
