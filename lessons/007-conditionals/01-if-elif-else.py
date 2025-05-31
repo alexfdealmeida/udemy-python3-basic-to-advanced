da8538496c244d2a4e4876e48b0f6a392782d241
alexfdealmeida <alexferreiradealmeida@gmail.com>
2025-05-31 20:11:50 -0300

    Branch: main; Added 01-if-elif-else.py

---
+++ lessons/007-conditionals/01-if-elif-else.py
@@ -0,0 +1,10 @@
+number = input("Enter number 1 or 2: ")
+
+number = int(number)
+
+if number == 1:
+	print("You entered option 1.")
+elif number == 2:
+	print("You entered option 2.")
+else:
+	print("Invalid option.")
